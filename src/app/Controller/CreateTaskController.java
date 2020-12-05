e096c0dcb5728b8acd19d45a179d477524c6a7f7
ryh <renyuhan519@gmail.com>
2020-12-05 14:45:21 +0800

    remove notif&priority

--- src/app/Controller/CreateTaskController.java
+++ src/app/Controller/CreateTaskController.java
@@ -17,16 +17,16 @@ public class CreateTaskController extends BaseController implements Initializabl
 
     @FXML
     VBox content;
-    @FXML
-    ToggleButton toggle;
+//    @FXML
+//    ToggleButton toggle;
     @FXML
     DatePicker dp1;
     @FXML
     TextField taskName;
     @FXML
     DatePicker dp2;
-    @FXML
-    ChoiceBox<Integer> choiceBox;
+//    @FXML
+//    ChoiceBox<Integer> choiceBox;
 
 
     StringConverter<LocalDate> converter = new StringConverter<LocalDate>() {
@@ -56,21 +56,21 @@ public class CreateTaskController extends BaseController implements Initializabl
     	dp2.setConverter(converter);
 
         //toggle.setBackground(new Background(new BackgroundImage(new Image("/fall3035/images/off.png"), null, null, null, new BackgroundSize(50, 45, true, true, true, true))));
-        toggle.setPrefWidth(55);
-        toggle.setPrefHeight(50);
-        toggle.selectedProperty().addListener((observable, oldValue, newValue) -> {
-            if (dp2.getValue() == null) {
-                showAlert("Notification can not be change when deadlineDate is empty!");
-                return;
-            }
-//            if (newValue) {
-//                toggle.setBackground(new Background(new BackgroundImage(new Image("/fall3035/images/on.png"), null, null, null, new BackgroundSize(50, 45, true, true, true, true))));
-//            } else {
-//                toggle.setBackground(new Background(new BackgroundImage(new Image("/fall3035/images/off.png"), null, null, null, new BackgroundSize(50, 45, true, true, true, true))));
+//        toggle.setPrefWidth(55);
+//        toggle.setPrefHeight(50);
+//        toggle.selectedProperty().addListener((observable, oldValue, newValue) -> {
+//            if (dp2.getValue() == null) {
+//                showAlert("Notification can not be change when deadlineDate is empty!");
+//                return;
 //            }
-        });
-        choiceBox.setItems(FXCollections.observableArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9));
-        choiceBox.getSelectionModel().select(0);
+////            if (newValue) {
+////                toggle.setBackground(new Background(new BackgroundImage(new Image("/fall3035/images/on.png"), null, null, null, new BackgroundSize(50, 45, true, true, true, true))));
+////            } else {
+////                toggle.setBackground(new Background(new BackgroundImage(new Image("/fall3035/images/off.png"), null, null, null, new BackgroundSize(50, 45, true, true, true, true))));
+////            }
+//        });
+//        choiceBox.setItems(FXCollections.observableArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+//        choiceBox.getSelectionModel().select(0);
     }
 
     @FXML
@@ -85,10 +85,13 @@ public class CreateTaskController extends BaseController implements Initializabl
             showAlert("Description can not be empty!");
             return;
         }
-        Integer priority = choiceBox.getSelectionModel().getSelectedItem();
+        //Integer priority = choiceBox.getSelectionModel().getSelectedItem();
+        Integer priority = 1;
 
         String deadlineDate = dp2.getValue() == null ? null : dp2.getValue().toString();
-        boolean isNotification = toggle.selectedProperty().get();
+        //boolean isNotification = toggle.selectedProperty().get();
+        boolean isNotification = false;
+
         if (deadlineDate != null && preferDate.isAfter(dp2.getValue())) {
             showAlert("DeadlineDate must more than PreferDate");
             return;
